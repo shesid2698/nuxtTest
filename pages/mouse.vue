051748a9307c06c437d231b6af7269c6f7f59b8d
shesid2698 <shesid2698@gmail.com>
2025-01-24 15:14:21 +0800

    add composables

--- pages/mouse.vue
+++ pages/mouse.vue
@@ -1,12 +1,27 @@
 <template>
-  <div>
-    <p>x軸 : {{mouse.x}}</p>
-    <p>y軸 : {{mouse.y}}</p>
-  </div>
+    <div>
+        <p>x軸 : {{mouse.x}}</p>
+        <p>y軸 : {{mouse.y}}</p>
+        <div class=" w-fit sid">
+            <ElIconApple class="w-30px"></ElIconApple>
+        </div>
+    </div>
 </template>
 <script setup lang="ts">
 const mouse = useMouse();
-onMounted(()=>{
+onMounted(() => {
+  window.addEventListener('mousemove',()=>{
+    const apple = document.querySelector('.sid') as HTMLElement;
+    if(apple ==null)return;
+    apple.style.top = `${mouse.y.value -15}px`;
+    apple.style.left = `${mouse.x.value -15}px`;
+  });
 });
 </script>
-<style scoped></style>
+<style scoped>
+.sid {
+    position: fixed;
+    top: 0;
+    left: 0;
+}
+</style>
